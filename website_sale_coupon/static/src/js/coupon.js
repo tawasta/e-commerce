93cc5c76ac1c31369ddbf120e3c25b60f1b7a6e1
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-04-13 15:11:40 +0300

    Migrate to 8.0

--- website_sale_coupon/static/src/js/coupon.js
+++ website_sale_coupon/static/src/js/coupon.js
@@ -1,39 +1,33 @@
-openerp.define('proposal', function (require) {
-    var _t = require('web.core')._t;
+$(function() {
+    // When coupon is submitted, try to use it and display error/success
+    $('#btn-coupon-submit').click(function(){
+        var action = "/shop/coupon/use";
+        var code = $('#coupon-code').val();
 
-    var ajax = require('web.ajax');
-
-    $(function() {
-        // When coupon is submitted, try to use it and display error/success
-        $('#btn-coupon-submit').click(function(){
-            var action = "/shop/coupon/use";
-            var code = $('#coupon-code').val();
-
-            ajax.jsonRpc(action, 'call', {'code': code}).then(function(data){
-                if(data.error){
-                    $('#coupon-code-error').text(data.error);
-                    // Hackish way to use show because Bootstrap has !important for .hidden
-                    $('#coupon-code-error').removeClass('hidden').hide().fadeIn();
-                } else {
-                    // Reload to update prices
-                    location.reload();
-                }
-            });
+        openerp.jsonRpc(action, 'call', {'code': code}).then(function(data){
+            if(data.error){
+                $('#coupon-code-error').text(data.error);
+                // Hackish way to use show because Bootstrap has !important for .hidden
+                $('#coupon-code-error').removeClass('hidden').hide().fadeIn();
+            } else {
+                // Reload to update prices
+                location.reload();
+            }
         });
-
-        // When deleting a product or a coupon product, remove the coupon log entry
-        $('.js_delete_product').click(function(){
-            var action = "/shop/coupon/remove";
-            var row = $(this).closest('tr');
-
-            var coupon_id = row.find('.js_coupon').attr('data-coupon-id');
-
-            var values = {'coupon_id': coupon_id};
-
-            ajax.jsonRpc(action, 'call', values).then(function(data){
-                // This could show an info message, but it shouldn't be necessary
-            });
-        });
-
     });
+
+    // When deleting a product or a coupon product, remove the coupon log entry
+    $('.js_delete_product').click(function(){
+        var action = "/shop/coupon/remove";
+        var row = $(this).closest('tr');
+
+        var coupon_id = row.find('.js_coupon').attr('data-coupon-id');
+
+        var values = {'coupon_id': coupon_id};
+
+        openerp.jsonRpc(action, 'call', values).then(function(data){
+            // This could show an info message, but it shouldn't be necessary
+        });
+    });
+
 });
