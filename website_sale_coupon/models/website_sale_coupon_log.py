44d66ba182260194fe9dd997f49f394aa12df518
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-04-15 16:09:53 +0300

    [REF] Formatting for website_sale_coupon

--- website_sale_coupon/models/website_sale_coupon_log.py
+++ website_sale_coupon/models/website_sale_coupon_log.py
@@ -1,19 +1,5 @@
 # -*- coding: utf-8 -*-
-
-# 1. Standard library imports:
-import random
-import string
-
-# 2. Known third party imports:
-
-# 3. Odoo imports (openerp):
-from odoo import api, fields, models
-
-# 4. Imports from Odoo modules:
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
+from odoo import fields, models
 
 
 class WebsiteSaleCouponLog(models.Model):
@@ -43,7 +29,7 @@ class WebsiteSaleCouponLog(models.Model):
 
     # 3. Default methods
 
-    # 4. Compute and search fields, in the same order that fields declaration
+    # 4. Compute and search fields
 
     # 5. Constraints and onchanges
 
