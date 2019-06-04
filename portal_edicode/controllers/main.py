c05b53df932520f79fbae481288479904b064318
Jarmo Kortetjärvi <jarmokortetjarvi@users.noreply.github.com>
2019-06-04 09:05:33 +0300

    [ADD] New module to show edicode and operator to user (#14)

---
+++ portal_edicode/controllers/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.website_portal.controllers.main import website_account
+
+
+class CustomerPortal(website_account):
+
+    def __init__(self):
+        super(CustomerPortal, self).__init__()
+
+        self.OPTIONAL_BILLING_FIELDS = \
+            self.OPTIONAL_BILLING_FIELDS + ['edicode', 'einvoice_operator']
