bf06e33b5b10e1d6a130669eb775eb7830eb91ec
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2020-02-24 17:06:16 +0200

    [REF] Formatting

--- website_sale_company_slider/__manifest__.py
+++ website_sale_company_slider/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Website Sale Company Slider",
     "summary": "Slider to checkout",
-    "version": "12.0.1.10",
+    "version": "12.0.1.1.0",
     "category": "Website",
     "website": "https://github.com/Tawasta/e-commerce",
     "author": "Tawasta",
--- website_sale_invoice_transmit_method/__manifest__.py
+++ website_sale_invoice_transmit_method/__manifest__.py
@@ -24,7 +24,7 @@
     'version': '12.0.1.0.2',
     'category': 'Website',
     'website': 'https://github.com/Tawasta/e-commerce',
-    'author': 'Oy Tawasta Technologies Ltd.',
+    'author': 'Tawasta',
     'license': 'AGPL-3',
     'application': False,
     'installable': True,
