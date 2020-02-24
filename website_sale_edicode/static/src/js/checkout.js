3e378ed068674a9db2850c8f7323066eea433b69
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2020-02-24 16:11:28 +0200

    [ADD] Toggle by invoice type

--- website_sale_edicode/static/src/js/checkout.js
+++ website_sale_edicode/static/src/js/checkout.js
@@ -2,6 +2,24 @@ odoo.define('website_sale_edicode.checkout', function (require) {
     "use strict";
 
     $(function() {
-        $('#einvoice_operator-select').select2();
+        $('#einvoice-operator-select').select2();
+
+        function toggleTransmitMethod() {
+            // If the selector exists, toggle visibility
+            console.log('ding');
+            console.log($("#customer-invoice-transmit-method"));
+            if($('#customer-invoice-transmit-method').length){
+                var transmit_method = $('#customer-invoice-transmit-method');
+                var code = transmit_method.find(':selected').data('code');
+
+                var einvoice = code == 'einvoice';
+                // Show einvoice address and operator if transmit method is einvoice
+                $('#einvoice-operator-div').toggleClass('d-none', !einvoice);
+                $('#edicode-div').toggleClass('d-none', !einvoice);
+            }
+        }
+
+        $('#customer-invoice-transmit-method').change(toggleTransmitMethod);
+        toggleTransmitMethod();
     });
 });
