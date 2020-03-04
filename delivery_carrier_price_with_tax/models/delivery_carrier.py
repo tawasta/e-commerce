682a40957181285cabaea9d70b8f6449845995a9
retropikzel <retropikzel@mailbox.org>
2020-03-04 09:16:04 +0200

    [ADD] New module

---
+++ delivery_carrier_price_with_tax/models/delivery_carrier.py
@@ -0,0 +1,22 @@
+from odoo import fields
+from odoo import models
+
+
+class DeliveryCarrier(models.Model):
+    _inherit = "delivery.carrier"
+
+    fixed_price_with_tax = fields.Float(
+        string="Price with tax", compute="_compute_price_with_tax",
+    )
+
+    def _compute_price_with_tax(self):
+        for record in self:
+            price = record.fixed_price
+            tax_amount = 0
+            for tax in record.product_id.taxes_id:
+                tax_ids = tax.compute_all(price, record.product_id.currency_id).get("taxes")
+
+                if tax_ids:
+                    tax_amount += tax_ids[0]["amount"]
+
+            record.price_with_tax = price + tax_amount
