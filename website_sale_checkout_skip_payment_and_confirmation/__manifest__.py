331877db272dd8108b7d85b10af056444c499152
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-05-15 13:03:26 +0300

    [IMP] Automatic commercial partner as customer when confirming

--- website_sale_checkout_skip_payment_and_confirmation/__manifest__.py
+++ website_sale_checkout_skip_payment_and_confirmation/__manifest__.py
@@ -21,7 +21,7 @@
 {
     'name': 'eCommerce - Skip Payment And order confirmation',
     'summary': 'Website orders will stay unconfirmed after sale process',
-    'version': '12.0.1.2.0',
+    'version': '12.0.1.3.0',
     'category': 'Website',
     'website': 'https://github.com/Tawasta/e-commerce',
     'author': 'Tawasta',
