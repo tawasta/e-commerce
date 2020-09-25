c877d0575aa70e3388f4b49c6f6d1bd2852a8a85
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2020-09-25 09:14:37 +0300

    Modified js

--- website_sale_company_slider/static/src/js/checkout.js
+++ website_sale_company_slider/static/src/js/checkout.js
@@ -34,6 +34,8 @@ odoo.define('website_sale_company_slider.checkout', function (require) {
 
             if (is_company === true) {
                 $('#is_company').attr('checked', 'checked');
+                $("label[for='company_name']").removeClass('d-none');
+                $("input[name='company_name']").removeClass('d-none');
                 if (required_fields.val().indexOf(',company_name,vat') < 0) {
                     required_fields.val(required_fields.val() + ',company_name,vat');
                 }
