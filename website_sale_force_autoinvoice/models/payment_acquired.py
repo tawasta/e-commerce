703ec099218c554a676d645a626c3466bb0d20cd
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-04-16 08:46:16 +0300

    [REF] Autoformat

--- website_sale_force_autoinvoice/models/payment_acquired.py
+++ website_sale_force_autoinvoice/models/payment_acquired.py
@@ -5,8 +5,12 @@ from odoo import models
 
 class PaymentAcquirer(models.Model):
 
-    _inherit = 'payment.acquirer'
+    _inherit = "payment.acquirer"
 
-    auto_confirm = fields.Selection([('allow', 'Allow'), ('not_allowed', 'Not allowed')], default='not_allowed')
+    auto_confirm = fields.Selection(
+        [("allow", "Allow"), ("not_allowed", "Not allowed")], default="not_allowed"
+    )
 
-    auto_create_invoice = fields.Selection([('allow', 'Allow'), ('not_allowed', 'Not allowed')], default='not_allowed')
+    auto_create_invoice = fields.Selection(
+        [("allow", "Allow"), ("not_allowed", "Not allowed")], default="not_allowed"
+    )
--- website_sale_force_autoinvoice/models/sale.py
+++ website_sale_force_autoinvoice/models/sale.py
@@ -6,11 +6,11 @@ from odoo import models
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
     def action_quotation_send(self):
-        if self.transaction_ids.acquirer_id.auto_confirm == 'allow':
+        if self.transaction_ids.acquirer_id.auto_confirm == "allow":
             self.sudo().action_confirm()
 
         return super(SaleOrder, self).action_quotation_send()
