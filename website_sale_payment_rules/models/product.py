25cdd1c605206193abb1eabb3692851a636a3953
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-09-20 08:33:28 +0300

    Modified field

--- website_sale_payment_rules/models/product.py
+++ website_sale_payment_rules/models/product.py
@@ -9,3 +9,6 @@ class ProductTemplate(models.Model):
     requires_attachment = fields.Boolean("Requires an attachment")
 
     requires_explanation = fields.Boolean("Requires an explanation")
+
+    company_id = fields.Many2one(
+        'res.company', 'Company', index=1, required=True)
