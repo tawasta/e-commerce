c5e728363a35b408cb56235b19b04ca0b3fa28c3
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-02-24 15:39:52 +0200

    [ADD] New module website_sale_domicile
    Add domicile to e-commerce checkout

---
+++ website_sale_domicile/static/src/js/checkout.js
@@ -0,0 +1,15 @@
+odoo.define("website_sale_domicile.checkout", function (require) {
+    "use strict";
+    var core = require("web.core");
+    var _t = core._t;
+
+    $(function () {
+        $("input[name=city]").blur(function() {
+            // Fill domicile after filling the city
+            if( !$("input[name=domicile]").val() ) {
+                $("input[name=domicile]").val($("input[name=city]").val())
+            }
+        });
+
+    });
+});
