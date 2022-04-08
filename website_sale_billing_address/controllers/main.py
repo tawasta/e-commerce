ce5ff45479eca583d3ca097a81b481d03ee39f25
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-04-08 15:20:29 +0300

    Modified views and functions

--- website_sale_billing_address/controllers/main.py
+++ website_sale_billing_address/controllers/main.py
@@ -1,4 +1,5 @@
 from odoo.http import request
+from urllib.parse import urlparse
 from odoo import http
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
@@ -32,13 +33,68 @@ class WebsiteSaleBilling(WebsiteSale):
     def extra_info(self, **post):
         order = request.website.sale_get_order()
         if order.use_different_billing_address and order.partner_id == order.partner_invoice_id:
+            print("=====KAYKO TAALLA VAI NYT=====")
             return request.redirect("/shop/billing_address")
         else:
             return super(WebsiteSaleBilling, self).extra_info(**post)
 
-    @http.route(['/shop/billing_address'], type='http', auth="public", website=True, sitemap=False)
-    def billing_address(self, **post):
+    def checkout_values(self, **kw):
+        response = super(WebsiteSaleBilling, self).checkout_values(**kw)
+        order = request.website.sale_get_order(force_create=1)
+        billings = []
+        if order.partner_id != request.website.user_id.sudo().partner_id:
+            Partner = order.partner_id.with_context(show_address=1).sudo()
+            childs = Partner.search([
+                ("id", "=", order.partner_id.id),
+                # ("type", "in", ["invoice"])
+            ]).mapped('child_ids')
+
+            billing_partners = Partner.search([
+                ('id', 'in', childs.ids),
+                ('type', '=', 'invoice'),
+            ], order='id desc')
+
+            print(order.partner_id)
+            print(billing_partners)
+            for bp in billing_partners:
+                if bp.is_company and bp.child_ids:
+                    for c in bp.child_ids:
+                        billings.append(c)
+                if bp.is_company and not bp.child_ids:
+                    billings.append(bp)
+                if not bp.is_company:
+                    billings.append(bp)
+
+            billings.append(order.partner_id)
+            print(billings)
+            response.update({'billings': billings})
+
+        return response
+
+    @http.route(['/shop/change/billing_address'], type='http', auth="public", website=True, sitemap=False)
+    def change_billing_address(self, **post):
         order = request.website.sale_get_order()
+
+        # check that cart is valid
+        order = request.website.sale_get_order()
+        redirection = self.checkout_redirection(order)
+        if redirection:
+            return redirection
+
+        if 'partner_id' in post and 'user_billing_address' in post:
+            partner_id = request.env["res.partner"].sudo().search([
+                ('id', '=', post.get('partner_id'))
+            ])
+            order.sudo().write({
+                'partner_invoice_id': partner_id
+            })
+
+            return request.redirect(post.get('return_url'))
+
+    @http.route(['/shop/billing_address', '/shop/billing_address/<int:partner_id>'], type='http', auth="public", website=True, sitemap=False)
+    def billing_address(self, partner_id=None, **post):
+        order = request.website.sale_get_order()
+        print(post)
 
         # check that cart is valid
         order = request.website.sale_get_order()
@@ -55,6 +111,7 @@ class WebsiteSaleBilling(WebsiteSale):
                 'phone': post.get("phone"),
                 'street': post.get("street"),
                 'street2': post.get("street2"),
+                'type': 'invoice',
                 'zip': post.get("zip"),
                 'city': post.get("city"),
                 'country_id': country.id,
@@ -64,6 +121,7 @@ class WebsiteSaleBilling(WebsiteSale):
                     'name': post.get("company_name"),
                     'email': post.get("company_email"),
                     'vat': post.get("vat"),
+                    'type': 'invoice',
                     'edicode': post.get("edicode") or False,
                     'einvoice_operator_id': post.get("einvoice_operator_id") or False
                 })
@@ -72,8 +130,8 @@ class WebsiteSaleBilling(WebsiteSale):
                 current_partner = request.env["res.partner"].sudo().search([
                     ('id', '=', post.get("partner_id"))
                 ])
-                if current_partner == order.partner_invoice_id:
-                    current_partner.sudo().write(partner_vals)
+
+                current_partner.sudo().write(partner_vals)
 
                 if post.get("company_id"):
                     current_company = request.env["res.partner"].sudo().search([
@@ -95,11 +153,16 @@ class WebsiteSaleBilling(WebsiteSale):
                     })
                     company = request.env["res.partner"].sudo().create(company_vals)
                     partner_id.sudo().write({'parent_id': company.id})
+                    company.sudo().write({'parent_id': order.partner_id.id})
+
+                else:
+                    partner_id.sudo().write({'parent_id': order.partner_id.id})
+
 
 
                 order.sudo().write({'partner_invoice_id': partner_id})
 
-            if current_user.partner_id == order.partner_id:
+            if current_user.partner_id == order.partner_id and not partner_id:
                 if not order.use_different_billing_address:
                     order.sudo().write({'use_different_billing_address': True})
             if 'submitted' in post and not 'editing' in post:
@@ -115,10 +178,28 @@ class WebsiteSaleBilling(WebsiteSale):
             'countries': countries,
             'order': order,
         }
-        if order.use_different_billing_address and order.partner_invoice_id != order.partner_id:
+        if 'new_billing_address' in post:
+            print("=====TAMA=====")
+            values.update({
+                'partner': False
+            })
+        if order.use_different_billing_address and order.partner_invoice_id != order.partner_id and not 'new_billing_address' in post:
             values.update({
                 'partner': order.partner_invoice_id
             })
+
+        if partner_id:
+            allow_partner_edit = False
+            edit_partner = request.env["res.partner"].sudo().search([
+                ('id', '=', partner_id)
+            ])
+            if edit_partner in order.partner_id.child_ids:
+                allow_partner_edit = True
+                values.update({
+                    'partner': edit_partner
+                })
+            else:
+                # TAHAN JOKIN VIRHE ETTÄ EI MUUTEN ONNISTU
 
         return request.render("website_sale_billing_address.billing_address", values)
 
