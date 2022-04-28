cd8ac98fdafbf6acce47cd34f9098897a58bd9a8
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-04-28 15:35:57 +0300

    Added validation

--- website_sale_company_email/controllers/main.py
+++ website_sale_company_email/controllers/main.py
@@ -1,4 +1,5 @@
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo import tools, _
 
 
 class WebsiteSale(WebsiteSale):
@@ -12,3 +13,18 @@ class WebsiteSale(WebsiteSale):
         res = super(WebsiteSale, self)._checkout_form_save(mode, checkout, all_values)
 
         return res
+
+    def checkout_form_validate(self, mode, all_form_values, data):
+        error = {}
+        error_message = []
+
+        # company email validation
+        if data.get('company_email') and not tools.single_email_re.match(data.get('company_email')):
+            error["company_email"] = 'error'
+            error_message.append(_('Invalid Company Email! Please enter a valid email address.'))
+
+            return error, error_message
+
+        return super(WebsiteSale, self).checkout_form_validate(
+            mode, all_form_values, data
+        )
