82fbcae20645ac191de4d0c5ab09c12b755e3863
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-06-03 09:01:16 +0300

    [FIX] Remember selection on errors

--- website_sale_edicode/__manifest__.py
+++ website_sale_edicode/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Website Sale Edicode",
     "summary": "Adds EDI code to website checkout form",
-    "version": "14.0.1.5.0",
+    "version": "14.0.1.5.1",
     "category": "Website",
     "website": "https://gitlab.com/tawasta/odoo/e-commerce",
     "author": "Tawasta",
