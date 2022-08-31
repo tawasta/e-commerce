8ba65dd1984e87bb14dc4f3ab2baedaae1288786
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-08-31 14:02:33 +0300

    [IMP] Add business code to partner kanban view

--- website_sale_business_code/__manifest__.py
+++ website_sale_business_code/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Finnish Business code to website sale",
     "summary": "Adds Finnish Business code to website checkout form",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Website",
     "website": "https://gitlab.com/tawasta/odoo/e-commerce",
     "author": "Tawasta",
