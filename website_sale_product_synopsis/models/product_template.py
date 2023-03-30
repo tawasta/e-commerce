04354fc67edad15952ecddbfa763958dfaefe837
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-03-30 15:53:30 +0300

    [MIG] 14.0 website_sale_product_synopsis

---
+++ website_sale_product_synopsis/models/product_template.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+
+    _inherit = "product.template"
+
+    website_synopsis = fields.Char(
+        string="Website synopsis",
+        help="Show product synopsis in website",
+        translate=True,
+    )
