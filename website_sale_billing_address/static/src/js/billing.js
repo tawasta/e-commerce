c3510e4a09279f968db760d34adc4eb82a4ae884
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-05-11 13:13:22 +0300

    Modified view

--- website_sale_billing_address/static/src/js/billing.js
+++ website_sale_billing_address/static/src/js/billing.js
@@ -6,7 +6,7 @@ odoo.define("website_sale_billing_address.billing_address", function () {
             var method = $(this).val();
             if (method == '1') {
                 $("#div_email").removeClass("d-none");
-                $('#email_input').addAttr('required');
+                $('#email_input').attr('required');
             } else {
                 $("#div_email").addClass("d-none");
                 $('#email_input').removeAttr('required');
