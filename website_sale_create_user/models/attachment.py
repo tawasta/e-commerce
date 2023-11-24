a9e3a0a6cefd00ca7094a70561ba80ab742b5472
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-11-24 07:28:28 +0200

    Run pre commit all files

--- website_sale_create_user/models/attachment.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class IrAttachment(models.Model):
-
-    _inherit = "ir.attachment"
-
-    membership_attachment = fields.Boolean(string="Membership attachment")
