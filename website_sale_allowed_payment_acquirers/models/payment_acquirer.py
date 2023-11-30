71d98d17f40f928f332fa864b1273dc16fcc72eb
difbit <timo.kekalainen@tawasta.fi>
2023-11-30 15:19:13 +0200

    [ADD] Hotifx: added website_sale_allowed_payment_acquirers module to the production branch

---
+++ website_sale_allowed_payment_acquirers/models/payment_acquirer.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class PaymentAcquirer(models.Model):
+
+    _inherit = "payment.acquirer"
+
+    allowed_group_ids = fields.Many2many(
+        string="Allowed groups", comodel_name="res.groups"
+    )
