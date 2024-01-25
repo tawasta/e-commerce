7b80181392a5cb51c5e1a24c50a2da5905500ade
difbit <timo.kekalainen@tawasta.fi>
2024-01-25 13:52:33 +0200

    [IMP] website_sale_show_sale_delay module: Added a new field for website delivery time
    Also run pre-commit to all files

--- website_sale_birth_year/controllers/main.py
+++ website_sale_birth_year/controllers/main.py
@@ -11,9 +11,11 @@ class WebsiteSale(WebsiteSale):
             response = super(WebsiteSale, self).address(**kw)
             if kw.get("birth_year"):
                 order = request.website.sale_get_order()
-                birth_year = request.env["res.birth.year"].sudo().search([
-                    ('id', '=', kw.get("birth_year"))
-                ])
+                birth_year = (
+                    request.env["res.birth.year"]
+                    .sudo()
+                    .search([("id", "=", kw.get("birth_year"))])
+                )
                 order.partner_id.sudo().write({"birth_year": birth_year.id})
         else:
             response = super(WebsiteSale, self).address(**kw)
