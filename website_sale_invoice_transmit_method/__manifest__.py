993cf3e4ab4b1f8184c23b2fea434ca06e045fda
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-02-14 10:03:52 +0200

    [IMP] Don't show transmit methods if there aren't any

--- website_sale_invoice_transmit_method/__manifest__.py
+++ website_sale_invoice_transmit_method/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Website Sale Account Invoice Transmit Method",
     "summary": "Adds account invoice transmit method to checkout",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Website",
     "website": "https://gitlab.com/tawasta/odoo/e-commerce",
     "author": "Tawasta",
