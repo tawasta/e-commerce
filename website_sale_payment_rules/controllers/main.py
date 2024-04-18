988bfb8038dfe2612e25b79256cf63fa3cb9009e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-04-18 09:24:12 +0300

    [REF] Remove obsolete debugs

--- website_sale_payment_rules/controllers/main.py
+++ website_sale_payment_rules/controllers/main.py
@@ -134,10 +134,7 @@ class WebsiteSale(WebsiteSale):
             unique_acquirers = list(set(acquirers_list))
 
             domain = expression.AND([[("id", "in", unique_acquirers)]])
-        logging.info("============DOMAIN=========")
-        logging.info(domain)
         acquirers = request.env["payment.acquirer"].search(domain)
-        logging.info(acquirers)
         values["acquirers"] = acquirers
 
         return values
