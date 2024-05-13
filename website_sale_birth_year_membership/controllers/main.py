6c90a8bebc32c4178f65d5fdaaed1c64e7cc8612
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-05-13 08:23:42 +0300

    [IMP] - run pre commit all files

--- website_sale_birth_year_membership/controllers/main.py
+++ website_sale_birth_year_membership/controllers/main.py
@@ -3,18 +3,21 @@ from odoo.http import request
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
+
 class WebsiteSale(WebsiteSale):
     @http.route()
     def address(self, **kw):
         response = super(WebsiteSale, self).address(**kw)
-        
+
         order = request.website.sale_get_order()
         has_membership = self._has_membership_product(order)
-        response.qcontext['has_membership_product'] = has_membership
+        response.qcontext["has_membership_product"] = has_membership
 
         if "submitted" in kw:
             if kw.get("year_of_birth"):
-                order.partner_id.sudo().write({"year_of_birth": kw.get("year_of_birth")})
+                order.partner_id.sudo().write(
+                    {"year_of_birth": kw.get("year_of_birth")}
+                )
 
         return response
 
--- website_sale_gender_membership/controllers/main.py
+++ website_sale_gender_membership/controllers/main.py
@@ -3,20 +3,23 @@ from odoo.http import request
 
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
+
 class WebsiteSale(WebsiteSale):
     @http.route()
     def address(self, **kw):
         response = super(WebsiteSale, self).address(**kw)
         order = request.website.sale_get_order()
-        
+
         has_membership_product = any(
-            line.product_id.membership for line in order.order_line if line.product_id.membership
+            line.product_id.membership
+            for line in order.order_line
+            if line.product_id.membership
         )
-        
+
         if "submitted" in kw:
             if kw.get("gender"):
                 order.partner_id.sudo().write({"gender": kw.get("gender")})
-                
+
         # Lisätään has_membership_product kontekstiin
-        response.qcontext['has_membership_product'] = has_membership_product
+        response.qcontext["has_membership_product"] = has_membership_product
         return response
