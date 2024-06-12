50c720ee9930c82446f230e194e228a89df13f4a
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-06-12 14:23:10 +0300

    [IMP] - mofiied function

--- website_sale_name_mode_handling/controllers/main.py
+++ website_sale_name_mode_handling/controllers/main.py
@@ -9,14 +9,15 @@ class CustomWebsiteSale(WebsiteSale):
     def address(self, **kw):
         if "submitted" in kw:
             if "name" in kw and kw.get("name"):
-                name_parts = kw.get("name").split(" ", 1)
-                if len(name_parts) == 2:
-                    firstname, lastname = name_parts
-                    kw["firstname"] = firstname
-                    kw["lastname"] = lastname
-                else:
-                    kw["firstname"] = " "
-                    kw["lastname"] = kw.get("name")
+                kw["name"] = kw.get("name")
+                #name_parts = kw.get("name").split(" ", 1)
+                # if len(name_parts) == 2:
+                #     firstname, lastname = name_parts
+                #     kw["firstname"] = firstname
+                #     kw["lastname"] = lastname
+                # else:
+                #     kw["firstname"] = " "
+                #     kw["lastname"] = kw.get("name")
             else:
                 name = request.env["res.partner"]._get_computed_name(
                     kw.get("lastname"), kw.get("firstname")
