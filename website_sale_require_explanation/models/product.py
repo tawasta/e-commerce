6023277bd4ed855c648cf4cce7513a47217b285b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-08-05 14:46:18 +0300

    [IMP] Better help text

--- website_sale_require_explanation/models/product.py
+++ website_sale_require_explanation/models/product.py
@@ -7,5 +7,6 @@ class ProductTemplate(models.Model):
     requires_explanation = fields.Boolean("Requires an explanation")
     requires_explanation_help = fields.Text(
         "Explanation text",
-        help="Show this help text if explanation is missing for this product",
+        translate=True,
+        help="Show this help text if order explanation is missing",
     )
