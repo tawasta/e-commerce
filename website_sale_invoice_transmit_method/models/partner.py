1f0432986b039e5956ae98cc2eadbccded13f500
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-08-13 10:03:02 +0300

    [FIX] Retaining transmit method when creating a company

--- website_sale_invoice_transmit_method/models/partner.py
+++ website_sale_invoice_transmit_method/models/partner.py
@@ -5,12 +5,10 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     def create_company(self):
+        transmit_method = self.customer_invoice_transmit_method_id
         res = super().create_company()
 
         if self.parent_id:
-            if not self.parent_id.customer_invoice_transmit_method_id:
-                # Add Transmit method to parent
-                self.parent_id.customer_invoice_transmit_method_id = (
-                    self.customer_invoice_transmit_method_id
-                )
+            # Add Transmit method to parent
+            self.parent_id.customer_invoice_transmit_method_id = transmit_method.id
         return res
