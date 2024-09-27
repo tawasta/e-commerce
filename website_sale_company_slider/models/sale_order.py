3455b7ff02b29b6508d8cd2bd81d2a9a889d607e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-09-27 09:52:05 +0300

    [IMP] Allow forcing company slider to company

--- website_sale_company_slider/models/sale_order.py
+++ website_sale_company_slider/models/sale_order.py
@@ -5,10 +5,18 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     hide_company_slider = fields.Boolean(
-        "Hide company slider", compute="_compute_hide_company_slider"
+        "Hide company slider",
+        compute="_compute_hide_company_slider",
+    )
+
+    force_company_slider = fields.Boolean(
+        "Force company slider", compute="_compute_hide_company_slider"
     )
 
     def _compute_hide_company_slider(self):
         for record in self:
-            hide = record.order_line.mapped("product_id.hide_company_slider")
-            record.hide_company_slider = True in hide
+            force_slider = record.order_line.mapped("product_id.force_company_slider")
+            hide_slider = record.order_line.mapped("product_id.hide_company_slider")
+
+            record.force_company_slider = True in force_slider
+            record.hide_company_slider = True in hide_slider or True in force_slider
