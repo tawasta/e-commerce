06696584abeeee915f12f5474e4be6d4c81bcb72
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-10-24 14:48:21 +0300

    [REF] Incorrect comment

--- website_sale_billing_address/controllers/main.py
+++ website_sale_billing_address/controllers/main.py
@@ -6,7 +6,7 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class WebsiteSaleBilling(WebsiteSale):
     def _get_mandatory_fields_billing(self, country_id=False):
-        """Remove email and phone from mandatory fields"""
+        """Remove email from mandatory fields"""
         res = super()._get_mandatory_fields_billing(country_id)
 
         if "email" in res:
