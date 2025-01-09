79b923f5863ff7c384a0b0c125af48a7e7434a58
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-01-09 09:06:00 +0200

    [IMP] prevent using addresses that could cause error 403
    - limit default invoice addresses only to those that are related to the partner's commercial partner. Others could cause an access error in checkout's address() step

--- website_sale_default_invoice_address/models/website.py
+++ website_sale_default_invoice_address/models/website.py
@@ -11,16 +11,35 @@ class Website(models.Model):
     def _prepare_sale_order_values(self, partner_sudo):
         self.ensure_one()
 
-        _logger.info("_prepare_sale_order_values reached")
+        partner_obj = self.env["res.partner"]
+
         values = super()._prepare_sale_order_values(partner_sudo)
+
+        invoice_address_to_use = False
 
         if partner_sudo.default_partner_invoice_id:
             # Check if partner has a default invoice address
-            values["partner_invoice_id"] = partner_sudo.default_partner_invoice_id.id
+            invoice_address_to_use = partner_sudo.default_partner_invoice_id.id
         elif partner_sudo.commercial_partner_id.default_partner_invoice_id:
             # Check if commercial partner has a default invoice address
-            values[
-                "partner_invoice_id"
-            ] = partner_sudo.commercial_partner_id.default_partner_invoice_id.id
+            invoice_address_to_use = (
+                partner_sudo.commercial_partner_id.default_partner_invoice_id.id
+            )
+
+        # Do a check to prevent using invoice addresses that would raise
+        # error 403 in website_sale's address()
+        if invoice_address_to_use:
+            allowed_invoice_addresses = partner_obj.search(
+                [("id", "child_of", partner_sudo.commercial_partner_id.ids)]
+            )
+
+            if invoice_address_to_use in [a.id for a in allowed_invoice_addresses]:
+                values["partner_invoice_id"] = invoice_address_to_use
+            else:
+                _logger.debug(
+                    "Skipped using invoice address id %s, partner %s "
+                    "did not have access to it "
+                    % (invoice_address_to_use, partner_sudo.id)
+                )
 
         return values
