178fe6b3b76afbc7c496b9d95a2f6c711cad83bf
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-09 12:11:37 +0300

    [IMP] - modified function

--- website_sale_invoicing_fee/controllers/main.py
+++ website_sale_invoicing_fee/controllers/main.py
@@ -1,63 +1,96 @@
-from odoo import http, _
-from odoo.exceptions import MissingError, AccessError, ValidationError
+from odoo import http
 from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.payment.controllers import portal as payment_portal
-from odoo import tools
-from odoo import Command
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
-class PaymentPortal(payment_portal.PaymentPortal):
-    @http.route()
-    def shop_payment_transaction(self, order_id, access_token, **kwargs):
-        # Haetaan tilaus ja tarkistetaan access
-        _logger.info(
-            f"shop_payment_transaction called with order_id={order_id}, access_token={access_token}, kwargs={kwargs}"
-        )
+class PortalWebsiteSale(payment_portal.PaymentPortal):
+    def _create_transaction(
+        self,
+        provider_id,
+        payment_method_id,
+        token_id,
+        amount,
+        currency_id,
+        partner_id,
+        flow,
+        tokenization_requested,
+        landing_route,
+        reference_prefix=None,
+        is_validation=False,
+        custom_create_values=None,
+        **kwargs,
+    ):
+        # Haetaan order custom_create_valuesista
+        order = None
+        if custom_create_values and "sale_order_ids" in custom_create_values:
+            sale_order_ids = custom_create_values.get("sale_order_ids")
+            # sale_order_ids voi olla esim [(6, 0, [order_id])]
+            if sale_order_ids and isinstance(sale_order_ids, (list, tuple)):
+                # Otetaan ensimmäinen order_id listasta
+                order_ids = []
+                # Etsitään ID:t listasta esim. [(6, 0, [38])] -> [38]
+                for cmd in sale_order_ids:
+                    if (
+                        isinstance(cmd, (list, tuple))
+                        and len(cmd) == 3
+                        and isinstance(cmd[2], (list, tuple))
+                    ):
+                        order_ids.extend(cmd[2])
+                if order_ids:
+                    order = request.env["sale.order"].sudo().browse(order_ids[0])
+                    if not order.exists():
+                        order = None
 
-        try:
-            order = self._document_check_access("sale.order", order_id, access_token)
-        except MissingError as error:
-            raise error
-        except AccessError:
-            raise ValidationError(_("The access token is invalid."))
-
-        if order.state == "cancel":
-            raise ValidationError(_("The order has been canceled."))
-
-        order._check_cart_is_ready_to_be_paid()
-
-        # Lisätään se oma lisäys ennen super-kutsua:
-        payment_method_id = kwargs.get("payment_method_id")
-        if payment_method_id:
-            product = (
-                request.env["payment.method"]
-                .sudo()
-                .browse(int(payment_method_id))
-                .product_id
+        if order:
+            payment_method = (
+                request.env["payment.method"].browse(payment_method_id)
+                if payment_method_id
+                else None
             )
-            if product and not any(
-                line.product_id.id == product.id for line in order.order_line
-            ):
-                product_desc = (
-                    f"[{product.default_code}] {product.name}"
-                    if product.default_code
-                    else product.name
-                )
-                request.env["sale.order.line"].sudo().create(
-                    {
-                        "order_id": order.id,
-                        "product_id": product.id,
-                        "product_uom_qty": 1,
-                        "price_unit": product.list_price,
-                        "name": product_desc,
-                        "company_id": order.company_id.id,
-                    }
-                )
+            product = payment_method.product_id if payment_method else None
 
-        # Kutsutaan alkuperäisen luokan metodia
-        result = super().shop_payment_transaction(order_id, access_token, **kwargs)
-        _logger.info(f"Super call returned: {result}")
-        return result
+            if product:
+                existing_line = any(
+                    line.product_id.id == product.id for line in order.order_line
+                )
+                if not existing_line:
+                    product_desc = (
+                        f"[{product.default_code}] {product.name}"
+                        if product.default_code
+                        else product.name
+                    )
+
+                    company_id = request.env.company.id
+                    request.env["sale.order.line"].sudo().create(
+                        {
+                            "customer_lead": 0,
+                            "product_id": product.id,
+                            "product_uom_qty": 1,
+                            "price_unit": product.list_price,
+                            "name": product_desc,
+                            "order_id": order.id,
+                            "company_id": company_id,
+                        }
+                    )
+            amount = order.amount_total
+
+        # Lopuksi kutsutaan alkuperäinen metodi
+        return super()._create_transaction(
+            provider_id,
+            payment_method_id,
+            token_id,
+            amount,
+            currency_id,
+            partner_id,
+            flow,
+            tokenization_requested,
+            landing_route,
+            reference_prefix=reference_prefix,
+            is_validation=is_validation,
+            custom_create_values=custom_create_values,
+            **kwargs,
+        )
