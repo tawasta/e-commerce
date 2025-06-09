5c4c84b7ac1696597e44138a12f999e511ecb895
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-09 09:32:47 +0300

    [IMP] - modified function

--- website_sale_invoicing_fee/controllers/main.py
+++ website_sale_invoicing_fee/controllers/main.py
@@ -1,38 +1,39 @@
 from odoo.addons.website_sale.controllers.main import WebsiteSale
-from odoo.http import request
-from odoo import http
+from odoo.http import request, route
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class WebsiteEventSale(WebsiteSale):
-    @http.route(
-        "/shop/payment", type="http", auth="public", website=True, sitemap=False
-    )
+
+    @route(['/shop/payment'], type='http', auth='public', website=True, sitemap=False)
     def shop_payment(self, **post):
         order = request.website.sale_get_order()
 
-        # Laskutustuotteen lisäys VAIN POST-pyynnöillä
-        if request.httprequest.method == "POST" and order:
-            transaction = order.transaction_ids[0]
-            product = transaction.payment_method_id.product_id
-            if product:
-                existing_product_ids = order.order_line.mapped("product_id").ids
-                if product.id not in existing_product_ids:
+        # Jatketaan vain jos tilaus on olemassa
+        if order and post:
+            _logger.info("===POST==== %s", post)
+            # Haetaan mahdollinen olemassa oleva transaktio
+            transaction = order.transaction_ids.filtered(lambda t: t.state in ('draft', 'pending'))
+            _logger.info("===TRANSACTION=== %s", transaction)
+            if transaction:
+                payment_method = transaction[0].payment_method_id
+                product = payment_method.product_id
+                if product and not any(line.product_id.id == product.id for line in order.order_line):
+                    # Lisää laskutustuote tilaukseen
                     product_desc = (
                         f"[{product.default_code}] {product.name}"
-                        if product.default_code
-                        else product.name
+                        if product.default_code else product.name
                     )
-                    request.env["sale.order.line"].sudo().create(
-                        {
-                            "customer_lead": 0,
-                            "product_id": product.id,
-                            "product_uom_qty": 1,
-                            "price_unit": product.list_price,
-                            "name": product_desc,
-                            "order_id": order.id,
-                            "company_id": request.website.company_id.id,
-                        }
-                    )
+                    request.env['sale.order.line'].sudo().create({
+                        'order_id': order.id,
+                        'product_id': product.id,
+                        'name': product_desc,
+                        'product_uom_qty': 1,
+                        'price_unit': product.list_price,
+                        'company_id': order.company_id.id,
+                    })
 
-        # Palautetaan superin normaali logiikka
+        # Lopuksi jatketaan alkuperäisellä logiikalla
         return super().shop_payment(**post)
