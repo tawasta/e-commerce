afdb654105f7e49d8d234b9384f98df10fd8685e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-09 09:49:45 +0300

    [IMP] - modified function

--- website_sale_invoicing_fee/controllers/main.py
+++ website_sale_invoicing_fee/controllers/main.py
@@ -1,45 +1,63 @@
-from odoo.addons.website_sale.controllers.main import WebsiteSale
-from odoo.http import request, route
+from odoo import http, _
+from odoo.exceptions import MissingError, AccessError, ValidationError
+from odoo.http import request
+from odoo.addons.payment.controllers import portal as payment_portal
+from odoo import tools
+from odoo import Command
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
-class WebsiteEventSale(WebsiteSale):
-    @route(["/shop/payment"], type="http", auth="public", website=True, sitemap=False)
-    def shop_payment(self, **post):
-        order = request.website.sale_get_order()
+class PaymentPortal(payment_portal.PaymentPortal):
+    @http.route()
+    def shop_payment_transaction(self, order_id, access_token, **kwargs):
+        # Haetaan tilaus ja tarkistetaan access
+        _logger.info(
+            f"shop_payment_transaction called with order_id={order_id}, access_token={access_token}, kwargs={kwargs}"
+        )
 
-        # Jatketaan vain jos tilaus on olemassa
-        if order and post:
-            _logger.info("===POST==== %s", post)
-            # Haetaan mahdollinen olemassa oleva transaktio
-            transaction = order.transaction_ids.filtered(
-                lambda t: t.state in ("draft", "pending")
+        try:
+            order = self._document_check_access("sale.order", order_id, access_token)
+        except MissingError as error:
+            raise error
+        except AccessError:
+            raise ValidationError(_("The access token is invalid."))
+
+        if order.state == "cancel":
+            raise ValidationError(_("The order has been canceled."))
+
+        order._check_cart_is_ready_to_be_paid()
+
+        # Lisätään se oma lisäys ennen super-kutsua:
+        payment_method_id = kwargs.get("payment_method_id")
+        if payment_method_id:
+            product = (
+                request.env["payment.method"]
+                .sudo()
+                .browse(int(payment_method_id))
+                .product_id
             )
-            _logger.info("===TRANSACTION=== %s", transaction)
-            if transaction:
-                payment_method = transaction[0].payment_method_id
-                product = payment_method.product_id
-                if product and not any(
-                    line.product_id.id == product.id for line in order.order_line
-                ):
-                    # Lisää laskutustuote tilaukseen
-                    product_desc = (
-                        f"[{product.default_code}] {product.name}"
-                        if product.default_code
-                        else product.name
-                    )
-                    request.env["sale.order.line"].sudo().create(
-                        {
-                            "order_id": order.id,
-                            "product_id": product.id,
-                            "name": product_desc,
-                            "product_uom_qty": 1,
-                            "price_unit": product.list_price,
-                            "company_id": order.company_id.id,
-                        }
-                    )
+            if product and not any(
+                line.product_id.id == product.id for line in order.order_line
+            ):
+                product_desc = (
+                    f"[{product.default_code}] {product.name}"
+                    if product.default_code
+                    else product.name
+                )
+                request.env["sale.order.line"].sudo().create(
+                    {
+                        "order_id": order.id,
+                        "product_id": product.id,
+                        "product_uom_qty": 1,
+                        "price_unit": product.list_price,
+                        "name": product_desc,
+                        "company_id": order.company_id.id,
+                    }
+                )
 
-        # Lopuksi jatketaan alkuperäisellä logiikalla
-        return super().shop_payment(**post)
+        # Kutsutaan alkuperäisen luokan metodia
+        result = super().shop_payment_transaction(order_id, access_token, **kwargs)
+        _logger.info(f"Super call returned: {result}")
+        return result
