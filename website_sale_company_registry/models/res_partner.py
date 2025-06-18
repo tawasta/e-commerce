26b85dc479f2906d8a72162f4a4e473e93d65f9b
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-06-18 19:53:39 +0300

    [ADD] new module website_sale_company_registry
    - based on 14.0 website_sale_business_code

---
+++ website_sale_company_registry/models/res_partner.py
@@ -0,0 +1,22 @@
+from odoo import api, models
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    def create_company(self):
+        # Set the company registry for parent company and also build VAT code from it
+
+        company_registry = self.company_registry
+
+        res = super().create_company()
+
+        if company_registry:
+            self.parent_id.company_registry = company_registry
+            self.parent_id._compute_vat_from_company_registry()
+
+        return res
