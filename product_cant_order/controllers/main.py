5f05ed52acfdee0c2d896d5add26eac2710171f9
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-30 11:31:15 +0300

    Add can_not_order field to template and use config param to choose if ordering check is done on product or template level

--- product_cant_order/controllers/main.py
+++ product_cant_order/controllers/main.py
@@ -1,7 +1,6 @@
 from odoo import http, exceptions, _
 from odoo.http import request
 
-
 class CheckProduct(http.Controller):
     @http.route(
         ["/check/product/<int:product_id>"],
@@ -15,12 +14,8 @@ class CheckProduct(http.Controller):
         Checks if the provided variant ID can be added to cart.
         """
 
-        product = (
-            request.env["product.product"]
-            .sudo()
-            .search([("id", "=", product_id)], limit=1)
-        )
-        if product:
-            return {"can_not_order": product.can_not_order}
+        product = request.env["product.product"].sudo().browse(product_id)
+        if product.exists():
+            return {"can_not_order": product.get_effective_can_not_order()}
         else:
             raise exceptions.ValidationError(_("Product ID %s not found", product_id))
