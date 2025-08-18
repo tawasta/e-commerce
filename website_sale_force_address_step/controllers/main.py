b4ec5bb4ba50f7439a9f37a1c9b0188d338d36bc
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-08-18 14:50:25 +0300

    [IMP] - run pre commit all files

--- website_sale_force_address_step/controllers/main.py
+++ website_sale_force_address_step/controllers/main.py
@@ -5,36 +5,33 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 _logger = logging.getLogger(__name__)
 
-class MyWebsiteSale(WebsiteSale):
 
+class MyWebsiteSale(WebsiteSale):
     @http.route()
     def shop_payment(self, **post):
-        if not request.session.get('checkout_done'):
-            return request.redirect('/shop/checkout')
+        if not request.session.get("checkout_done"):
+            return request.redirect("/shop/checkout")
         return super().shop_payment(**post)
 
     @http.route()
     def checkout(self, **post):
         response = super().checkout(**post)
         if not post:
-            request.session['checkout_done'] = True
+            request.session["checkout_done"] = True
         return response
-
 
     @http.route()
     def confirm_order(self, **post):
-        if not request.session.get('checkout_done'):
-            return request.redirect('/shop/checkout')
+        if not request.session.get("checkout_done"):
+            return request.redirect("/shop/checkout")
         return super().confirm_order(**post)
-
 
     @http.route()
     def shop_payment_confirmation(self, **post):
         response = super().shop_payment_confirmation(**post)
 
         # Kun maksu vahvistettu, poistetaan checkout_done
-        if request.session.get('checkout_done'):
-            request.session.pop('checkout_done', None)
+        if request.session.get("checkout_done"):
+            request.session.pop("checkout_done", None)
 
         return response
-
