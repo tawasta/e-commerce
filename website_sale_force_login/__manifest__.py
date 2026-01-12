6d51394c97a1b42cb09bfef55d17e7738378c83d
Timo Talvitie <timo.talvitie@tawasta.fi>
2026-01-12 09:28:42 +0200

    [FIX] add t-nocache attribute to is_public_user() check
    - previously the check could give a wrong result when e.g. portal user reloaded the page rapidly multiple times

--- website_sale_force_login/__manifest__.py
+++ website_sale_force_login/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "eCommerce: Force login",
     "summary": "Force login before adding products to carts",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.2.0",
     "category": "Website",
     "website": "https://github.com/tawasta/e-commerce",
     "author": "Futural",
