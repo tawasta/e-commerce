5d7a1495f15cf70f0f2a563946c813a32942e94e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-01-26 13:08:49 +0200

    [IMP] - run pre commit all files

--- woo_export_stock_cron/models/woo_product_template_ept.py
+++ woo_export_stock_cron/models/woo_product_template_ept.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 import logging
+
 from odoo import api, models
 from odoo.tools.misc import split_every
 
@@ -11,12 +11,18 @@ class WooProductTemplateEpt(models.Model):
 
     @api.model
     def cron_export_stock_in_woo(self):
-        templates = self.sudo().search([
-            ("exported_in_woo", "=", True),
-            ("active", "=", True),
-            ("woo_instance_id.active", "=", True),
-            ("woo_tmpl_id", "!=", False),
-        ]).ids
+        templates = (
+            self.sudo()
+            .search(
+                [
+                    ("exported_in_woo", "=", True),
+                    ("active", "=", True),
+                    ("woo_instance_id.active", "=", True),
+                    ("woo_tmpl_id", "!=", False),
+                ]
+            )
+            .ids
+        )
 
         if not templates:
             _logger.info("Woo stock export cron: nothing to export.")
@@ -30,7 +36,7 @@ class WooProductTemplateEpt(models.Model):
                 active_model="woo.product.template.ept",
                 active_ids=chunk,
             )
-            wiz = Wizard.with_context(ctx).create({})
-            wiz.with_context(ctx).export_stock_in_woo()
+            wiz = Wizard.with_context(**ctx).create({})
+            wiz.with_context(**ctx).export_stock_in_woo()
 
         return True
